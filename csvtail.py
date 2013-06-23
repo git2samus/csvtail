d60460b8166125a2726eb7834c487ae1b865915f
Michael Cetrulo <mail2samus@gmail.com>
2013-06-23 17:00:28 -0300

    keep rowindex

--- csvtail.py
+++ csvtail.py
@@ -5,21 +5,27 @@ from curses import wrapper
 class GenRows(object):
     def __init__(self, f):
         self.f = f
-        self.rowpos = 0
-        self.rowlen = 0
+        self.rowindex = []
+        self._rowpos = 0
+        self._rowlen = 0
+
+    @property
+    def rowpos(self):
+        return self._rowpos
+
+    def _linegen(self):
+        """ auxiliary generator to return lines for csv.reader while tracking line length """
+        for line in self.f:
+            self._rowlen += len(line)
+            yield line
 
     def __iter__(self):
         """ yield csv rows from file while updating rowpos index """
-        def linegen():
-            """ auxiliary generator to return lines for csv.reader while tracking line length """
-            for line in self.f:
-                self.rowlen += len(line)
-                yield line
-
-        for row in csv.reader(linegen()):
+        for row in csv.reader(self._linegen()):
+            self.rowindex.append(self._rowpos)
             yield row
-            self.rowpos += self.rowlen
-            self.rowlen = 0
+            self._rowpos += self._rowlen
+            self._rowlen = 0
 
 
 '''
@@ -43,4 +49,5 @@ if __name__ == '__main__':
         genrows = GenRows(f)
         for row in genrows:
             print genrows.rowpos, row
+        print genrows.rowindex
 
