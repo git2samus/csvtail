720e783d82b82635169dc4c512d33fa2d80181a2
Michael Cetrulo <mail2samus@gmail.com>
2013-07-28 16:16:52 -0300

    respond to movement

--- csvtail.py
+++ csvtail.py
@@ -14,13 +14,15 @@ def with_curses(func):
 
 @with_curses
 def main(stdscr, csv_rows):
-    def update_src():
+    def update_scr():
         stdscr.clear()
-        for scr_row_i in range(num_scr_rows):
-            stdscr.addstr(scr_row_i, 0, '{}'.format(scr_row_i))
+        stdscr.addstr(0, 0, '{}-{} ({}x{})'.format(
+            csv_row_offset, csv_col_offset,
+            num_scr_rows, num_scr_cols,
+        ))
 
     # total rows
-    #num_csv_rows = len(csv_rows)
+    num_csv_rows = len(csv_rows)
     # calculate maximum width of each column
     csv_colwidths = []
     for csv_row in csv_rows:
@@ -34,25 +36,55 @@ def main(stdscr, csv_rows):
     # get (current) screen dimensions
     num_scr_rows, num_scr_cols = stdscr.getmaxyx()
     # row and col from which the CSV should start printing
-    #csv_offset_row, csv_offset_col = 0, 0
+    csv_row_offset, csv_col_offset = 0, 0
 
     # keys we'll respond to, grouped by action
+    #TODO ESC has delay for escape sequences
     action_keys = {
-        'quit':   ('q',),
-        'resize': ('KEY_RESIZE',),
+        'quit':   (ord('q'),),
+        'resize': (curses.KEY_RESIZE,),
+        'up':     (ord('k'), curses.KEY_UP,),
+        'down':   (ord('j'), curses.KEY_DOWN,),
+        'left':   (ord('h'), curses.KEY_LEFT,),
+        'right':  (ord('l'), curses.KEY_RIGHT,),
     }
+    movement_keys = set(
+        key
+        for action in ('up', 'down', 'left', 'right')
+        for key in action_keys[action]
+    )
+
+    # initial screen
+    update_scr()
 
     # main loop
     while True:
-        update_src()
+        key = stdscr.getch()  # this calls stdscr.refresh()
 
-        key = stdscr.getkey()
         if key in action_keys['quit']:
-            break
-
-        if key in action_keys['resize']:
+            break  # exit main loop
+        elif key in action_keys['resize']:
             # update screen dimensions
             num_scr_rows, num_scr_cols = stdscr.getmaxyx()
+            # mark screen for redraw to avoid clipping issues
+            curses.doupdate()
+        elif key in movement_keys:
+            #TODO else offset (curses.flash/curses.beep?)
+            if key in action_keys['up']:
+                if csv_row_offset > 0:
+                    csv_row_offset -= 1
+            elif key in action_keys['down']:
+                if csv_row_offset < num_csv_rows - 1:
+                    csv_row_offset += 1
+            elif key in action_keys['left']:
+                if csv_col_offset > 0:
+                    csv_col_offset -= 1
+            elif key in action_keys['right']:
+                if csv_col_offset < len(csv_colwidths) - 1:
+                    csv_col_offset += 1
+
+            # update screen buffer
+            update_scr()
 
 
 if __name__ == '__main__':
